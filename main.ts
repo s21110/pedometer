a6fd1ac2ba13a591e8f3ea1373ef7b2eb1bb3dd8
s21110 <122764722+s21110@users.noreply.github.com>
2023-03-01 13:29:19 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+    basic.showNumber(counter)
 })
+input.onButtonPressed(Button.B, function () {
+    counter = 0
+})
+let counter = 0
+counter = 0
